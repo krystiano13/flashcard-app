77ef788619ca4ff4a710fe6163989b4aac3ba29a
krystiano13 <crystianotv@gmail.com>
2024-03-11 13:59:31 +0100

    implement nav buttons' navigation

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -12,23 +12,32 @@ export function Home() {
     const [searchOpen, setSearchOpen] = useState(false);
     return (
         <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between">
-            <section id="search" className="flex justify-end items-center p-5">
+            <section id="search" className="w-full bg flex items-center justify-end p-5">
                 <button
                     onClick={() => setSearchOpen(prev => !prev)}
-                    className={`w-10 p-1.5 rounded-xl ${searchOpen && 'chosen'}`}
+                    className={`${searchOpen && 'chosen'} nav-button w-10 p-1.5 rounded-xl`}
                 >
                     <img src={searchIcon} alt="search icon" />
                 </button>
             </section>
             <section id="content"></section>
             <nav className="w-full flex items-center justify-evenly p-3 glassomorph1">
-                <button onClick={() => setIconId(0)} className={`${iconId === 0 && 'chosen'} w-14 p-1.5 rounded-xl`}>
+                <button
+                    onClick={() => setIconId(0)}
+                    className={`${iconId === 0 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+                >
                     <img src={homeIcon} alt="home icon" />
                 </button>
-                <button onClick={() => setIconId(1)} className={`${iconId === 1 && 'chosen'} w-14 p-1.5 rounded-xl`}>
+                <button
+                    onClick={() => setIconId(1)}
+                    className={`${iconId === 1 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+                >
                     <img src={addIcon} alt="plus icon"/>
                 </button>
-                <button onClick={() => setIconId(2)} className={`${iconId === 2 && 'chosen'} w-14 p-1.5 rounded-xl`}>
+                <button
+                    onClick={() => setIconId(2)}
+                    className={`${iconId === 2 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+                >
                     <img src={settingsIcon} alt="settings icon"/>
                 </button>
             </nav>
