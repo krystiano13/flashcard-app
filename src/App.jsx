13e77006f3e6c57ca00b6fad5022c211a73595d6
krystiano13 <crystianotv@gmail.com>
2024-03-11 11:08:20 +0100

    add fonts and icons assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 function App () {
   return (
-    <div>
-      <h1 className="text-center">Flashcard App</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <h1 className="text-center">Flashcard App</h1>
+            <Link to='/about'>About</Link>
+          </div>
+        } />
+        <Route path="/about" element={
+          <div>
+            <h1 className="text-center">About</h1>
+            <Link to='/'>Home</Link>
+          </div>
+        } />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
