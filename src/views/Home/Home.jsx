e3c2a079898c390d418ae514e7bab76c60c75d91
krystiano13 <crystianotv@gmail.com>
2024-03-11 13:33:03 +0100

    finish styles for navbar

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,22 +1,28 @@
+// react-imports
+import { useState } from "react";
+
+// assets
 import searchIcon from '../../../public/icons/search-icon.svg';
 import homeIcon from '../../../public/icons/home-icon.svg';
 import settingsIcon from '../../../public/icons/settings-icon.svg';
 import addIcon from '../../../public/icons/plus-icon.svg';
+
 export function Home() {
+    const [iconId, setIconId] = useState(0);
     return (
-        <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between p-2">
-            <section id="search">
+        <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between">
+            <section id="search" className="flex justify-end items-center">
 
             </section>
             <section id="content"></section>
             <nav className="w-full flex items-center justify-evenly p-3 glassomorph1">
-                <button className="w-14 choosen p-1.5 rounded-xl">
+                <button onClick={() => setIconId(0)} className={`${iconId === 0 && 'chosen'} w-14 p-1.5 rounded-xl`}>
                     <img src={homeIcon} alt="home icon" />
                 </button>
-                <button className="w-14 p-1.5">
+                <button onClick={() => setIconId(1)} className={`${iconId === 1 && 'chosen'} w-14 p-1.5 rounded-xl`}>
                     <img src={addIcon} alt="plus icon"/>
                 </button>
-                <button className="w-14 p-1.5">
+                <button onClick={() => setIconId(2)} className={`${iconId === 2 && 'chosen'} w-14 p-1.5 rounded-xl`}>
                     <img src={settingsIcon} alt="settings icon"/>
                 </button>
             </nav>
