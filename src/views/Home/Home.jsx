8cf2eaefc852a6a1f6d1be5408099ce825bd90ca
krystiano13 <crystianotv@gmail.com>
2024-03-11 13:20:46 +0100

    add color palette

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,19 +4,19 @@ import settingsIcon from '../../../public/icons/settings-icon.svg';
 import addIcon from '../../../public/icons/plus-icon.svg';
 export function Home() {
     return (
-        <main className="w-[100vw] h-[100vh] bg-gray-800 flex flex-col justify-between p-2">
+        <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between p-2">
             <section id="search">
 
             </section>
             <section id="content"></section>
             <nav className="w-full flex items-center justify-evenly p-3 glassomorph1">
-                <button className="w-12">
+                <button className="w-14 choosen p-1.5 rounded-xl">
                     <img src={homeIcon} alt="home icon" />
                 </button>
-                <button className="w-12">
+                <button className="w-14 p-1.5">
                     <img src={addIcon} alt="plus icon"/>
                 </button>
-                <button className="w-12">
+                <button className="w-14 p-1.5">
                     <img src={settingsIcon} alt="settings icon"/>
                 </button>
             </nav>
