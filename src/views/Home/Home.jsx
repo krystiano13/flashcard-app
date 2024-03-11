95df6e633d3eac1138d830f92f95327ef64a6f27
krystiano13 <crystianotv@gmail.com>
2024-03-11 13:04:40 +0100

    implement glassomorphism to the navbar

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -4,19 +4,19 @@ import settingsIcon from '../../../public/icons/settings-icon.svg';
 import addIcon from '../../../public/icons/plus-icon.svg';
 export function Home() {
     return (
-        <main className="w-[100vw] h-[100vh] bg-gray-800 flex flex-col justify-between p-1">
+        <main className="w-[100vw] h-[100vh] bg-gray-800 flex flex-col justify-between p-2">
             <section id="search">
 
             </section>
             <section id="content"></section>
-            <nav className="w-full flex items-center justify-evenly p-3">
-                <button className="w-16">
+            <nav className="w-full flex items-center justify-evenly p-3 glassomorph1">
+                <button className="w-12">
                     <img src={homeIcon} alt="home icon" />
                 </button>
-                <button className="w-16">
+                <button className="w-12">
                     <img src={addIcon} alt="plus icon"/>
                 </button>
-                <button className="w-16">
+                <button className="w-12">
                     <img src={settingsIcon} alt="settings icon"/>
                 </button>
             </nav>
