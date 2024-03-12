3ab0e0519f0ef9924a6bbdeb0d8c46e90bffe128
krystiano13 <crystianotv@gmail.com>
2024-03-12 11:35:01 +0100

    move to typescript

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,12 +2,15 @@
 import { useState } from "react";
 
 //assets
+//@ts-ignore
 import homeIcon from "../../public/icons/home-icon.svg";
+//@ts-ignore
 import addIcon from "../../public/icons/plus-icon.svg";
+//@ts-ignore
 import settingsIcon from "../../public/icons/settings-icon.svg";
 
 export function Nav() {
-    const [iconId, setIconId] = useState(0);
+    const [iconId, setIconId] = useState<number>(0);
     return (
         <nav className="w-full flex h-[10vh] items-center justify-evenly p-5 glassomorph1 rounded-tl-2xl rounded-tr-2xl border-t-[1px] border-solid">
             <button
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -2,14 +2,15 @@
 import { useState } from "react";
 
 // components
-import { Nav } from '../../components/Nav.jsx';
-import { DeckCard } from "../../components/Home/DeckCard.jsx";
+import { Nav } from '../../components/Nav.js';
+import { DeckCard } from "../../components/Home/DeckCard.js";
 
 // assets
+// @ts-ignore
 import searchIcon from '../../../public/icons/search-icon.svg';
 
 export function Home() {
-    const [searchOpen, setSearchOpen] = useState(false);
+    const [searchOpen, setSearchOpen] = useState<boolean>(false);
     return (
         <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between">
             <section id="search" className="w-full h-[10vh] bg flex items-center justify-end p-5">
