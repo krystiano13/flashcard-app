00bed6cda72fe22b7c6f93df5278491143d5dd8b
krystiano13 <crystianotv@gmail.com>
2024-03-12 21:26:03 +0100

    add 4th button at navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,5 @@
 // react imports
-import { useState } from "react";
+import React, { useState } from "react";
 
 //assets
 //@ts-ignore
@@ -8,11 +8,14 @@ import homeIcon from "../../public/icons/home-icon.svg";
 import addIcon from "../../public/icons/plus-icon.svg";
 //@ts-ignore
 import settingsIcon from "../../public/icons/settings-icon.svg";
+//@ts-ignore
+import bookIcon from '../../public/icons/book-icon.png';
 
 export function Nav() {
     const [iconId, setIconId] = useState<number>(0);
     return (
-        <nav className="w-full flex h-[10vh] items-center justify-evenly p-5 glassomorph1 rounded-tl-2xl rounded-tr-2xl border-t-[1px] border-solid">
+        <nav
+            className="w-full flex h-[10vh] items-center justify-evenly p-5 glassomorph1 rounded-tl-2xl rounded-tr-2xl border-t-[1px] border-solid">
             <button
                 onClick={() => setIconId(0)}
                 className={`${iconId === 0 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
@@ -23,11 +26,17 @@ export function Nav() {
                 onClick={() => setIconId(1)}
                 className={`${iconId === 1 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
             >
-                <img src={addIcon} alt="plus icon"/>
+                <img src={bookIcon} alt="book icon"/>
             </button>
             <button
                 onClick={() => setIconId(2)}
                 className={`${iconId === 2 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+            >
+                <img src={addIcon} alt="plus icon"/>
+            </button>
+            <button
+                onClick={() => setIconId(3)}
+                className={`${iconId === 3 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
             >
                 <img src={settingsIcon} alt="settings icon"/>
             </button>
