56846380c9699414fcdef6d4336d5546ac363a3f
krystiano13 <crystianotv@gmail.com>
2024-03-12 11:14:15 +0100

    extract component from navbar

---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import homeIcon from "../../public/icons/home-icon.svg";
+import addIcon from "../../public/icons/plus-icon.svg";
+import settingsIcon from "../../public/icons/settings-icon.svg";
+
+export function Nav() {
+    const [iconId, setIconId] = useState(0);
+    return (
+        <nav className="w-full flex items-center justify-evenly p-3 glassomorph1">
+            <button
+                onClick={() => setIconId(0)}
+                className={`${iconId === 0 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+            >
+                <img src={homeIcon} alt="home icon"/>
+            </button>
+            <button
+                onClick={() => setIconId(1)}
+                className={`${iconId === 1 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+            >
+                <img src={addIcon} alt="plus icon"/>
+            </button>
+            <button
+                onClick={() => setIconId(2)}
+                className={`${iconId === 2 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
+            >
+                <img src={settingsIcon} alt="settings icon"/>
+            </button>
+        </nav>
+    )
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,13 @@
 // react-imports
 import { useState } from "react";
 
+// components
+import { Nav } from '../../components/Nav.jsx';
+
 // assets
 import searchIcon from '../../../public/icons/search-icon.svg';
-import homeIcon from '../../../public/icons/home-icon.svg';
-import settingsIcon from '../../../public/icons/settings-icon.svg';
-import addIcon from '../../../public/icons/plus-icon.svg';
 
 export function Home() {
-    const [iconId, setIconId] = useState(0);
     const [searchOpen, setSearchOpen] = useState(false);
     return (
         <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between">
@@ -20,27 +19,10 @@ export function Home() {
                     <img src={searchIcon} alt="search icon" />
                 </button>
             </section>
-            <section id="content"></section>
-            <nav className="w-full flex items-center justify-evenly p-3 glassomorph1">
-                <button
-                    onClick={() => setIconId(0)}
-                    className={`${iconId === 0 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
-                >
-                    <img src={homeIcon} alt="home icon" />
-                </button>
-                <button
-                    onClick={() => setIconId(1)}
-                    className={`${iconId === 1 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
-                >
-                    <img src={addIcon} alt="plus icon"/>
-                </button>
-                <button
-                    onClick={() => setIconId(2)}
-                    className={`${iconId === 2 && 'chosen'} nav-button w-14 p-1.5 rounded-xl`}
-                >
-                    <img src={settingsIcon} alt="settings icon"/>
-                </button>
-            </nav>
+            <section id="content">
+
+            </section>
+            <Nav />
         </main>
     )
 }
