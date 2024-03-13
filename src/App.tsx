4af9fe1737b529c941f19e7f907f6186fe14d6e4
krystiano13 <crystianotv@gmail.com>
2024-03-13 10:55:51 +0100

    create context for data about cards and decks

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, {createContext, useState} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./views/Home/Home";
 import { Learn } from "./views/Learn/Learn";
 import { Nav } from "./components/Nav";
 
+import type { deck } from "./types/types";
+
+export const DeckContext = createContext<deck[]>([]);
+
 function App () {
-  return (
+    const [decks, setDecks] = useState<deck[]>([
+        {
+            id: 0,
+            title: "Default Deck",
+            cardsInDeck: 2,
+            cards: [
+                {
+                    id: 0,
+                    oneSide: "one side",
+                    secondSide: "second side",
+                    whenToSee: new Date(new Date().getTime() - (1000 * 60 * 60 * 24))
+                }
+            ]
+        },
+    ]);
+    return (
       <main className="w-[100vw] h-[100vh] bg flex flex-col justify-between">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/learn" element={<Learn />} />
-            </Routes>
-            <Nav />
-        </BrowserRouter>
+          <DeckContext.Provider value={decks}>
+              <BrowserRouter>
+                  <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/learn" element={<Learn />} />
+                  </Routes>
+                  <Nav />
+              </BrowserRouter>
+          </DeckContext.Provider>
       </main>
   )
 }
--- src/views/Learn/Learn.tsx
+++ src/views/Learn/Learn.tsx
@@ -1,24 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { DeckContext } from "../../App";
 import { DeckCard } from "../../components/Home/DeckCard";
 import type { deck } from "../../types/types";
 
-export function Learn() {
+export const Learn = ( ) => {
     const [decksToSee, setDecksToSee] = useState<deck[]>([]);
-    const [decks, setDecks] = useState<deck[]>([
-        {
-            id: 0,
-            title: "Default Deck",
-            cardsInDeck: 2,
-            cards: [
-                {
-                    id: 0,
-                    oneSide: "one side",
-                    secondSide: "second side",
-                    whenToSee: new Date(new Date().getTime() - (1000 * 60 * 60 * 24))
-                }
-            ]
-        },
-    ]);
+    const decks = useContext(DeckContext);
 
     useEffect(() => {
         const decksArray: deck[] = decks;
