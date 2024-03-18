ff85a25102d159db9b4bc137c53b9f2aeeb807f7
krystiano13 <crystianotv@gmail.com>
2024-03-18 09:58:19 +0100

    create styles for deck-create form

--- src/views/DeckCreate/DeckCreate.tsx
+++ src/views/DeckCreate/DeckCreate.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 export function DeckCreate() {
     return (
-        <></>
+        <section id="content" className="h-[90vh] p-3 flex justify-center items-center appear1">
+            <form className="w-[90%] p-3 gap-12 rounded-lg flex flex-col justify-center items-center">
+                <input
+                    className="w-full bg-transparent text-lg md:text-xl p-1 text-white outline-0 border-b-solid border-b-[1px] border-white"
+                    placeholder="Your deck name"
+                    name="deck"
+                    type="text"
+                    required
+                    max={64}
+                />
+                <button
+                    className="gradient1 font-semibold w-[90%] text-white p-2 pl-6 pr-6 rounded-xl"
+                    type="submit"
+                >
+                    Create
+                </button>
+            </form>
+        </section>
     )
 }
