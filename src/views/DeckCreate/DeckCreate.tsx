f92462e4aa20496f5b908dece50eaf1b2631cd28
krystiano13 <crystianotv@gmail.com>
2024-03-18 10:45:50 +0100

    make deck-create form look better on tablets

--- src/views/DeckCreate/DeckCreate.tsx
+++ src/views/DeckCreate/DeckCreate.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 export function DeckCreate() {
     return (
         <section id="content" className="h-[90vh] p-3 flex justify-center items-center">
-            <form className="w-[90%] p-3 gap-12 rounded-lg flex flex-col justify-center items-center appear2">
+            <form className="w-[90%] md:w-[70%] p-3 gap-12 rounded-lg flex flex-col justify-center items-center appear2">
                 <input
-                    className="w-full bg-transparent text-lg md:text-xl pb-2 text-white outline-0 border-b-solid border-b-[1px] border-white"
+                    className="
+                    w-full bg-transparent text-lg md:text-3xl pb-2 md:pb-4 text-white outline-0
+                    border-b-solid border-b-[1px] border-white "
                     placeholder="Your deck name"
                     name="deck"
                     type="text"
@@ -12,7 +14,8 @@ export function DeckCreate() {
                     max={64}
                 />
                 <button
-                    className="gradient1 font-semibold w-[90%] text-white p-2 pl-6 pr-6 rounded-xl"
+                    className="gradient1 font-semibold w-[90%] md:rounded-lg
+                    text-white md:text-xl p-2 md:p-3 pl-6 md:pl-9 pr-6 md:pr-9 rounded-xl"
                     type="submit"
                 >
                     Create
