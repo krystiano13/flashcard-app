f37527f387399c760852566f89c7ae92c2706c38
krystiano13 <crystianotv@gmail.com>
2024-03-18 10:22:48 +0100

    make deck-card component look good on tablets

--- src/components/Home/DeckCard.tsx
+++ src/components/Home/DeckCard.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 export const DeckCard:FC<Props> = ({ title, cardsInDeck, mode }) => {
     return (
-        <button className="w-full appear2 glassomorph1 flex flex-col gap-2 rounded-lg border-solid border-[1px] p-2">
-            <h2 className="text-txt text-2xl font-semibold">{ title }</h2>
-            <p className="text-txt text-lg font-light">{ mode === "edit" ? "Cards In Deck": "Cards To See" }: { cardsInDeck.toString() }</p>
+        <button className="w-full appear2 glassomorph1 flex flex-col gap-2 md:gap-4 rounded-lg border-solid border-[1px] p-2">
+            <h2 className="text-txt text-2xl md:text-4xl font-semibold">{ title }</h2>
+            <p className="text-txt text-lg md:text-2xl font-light">{ mode === "edit" ? "Cards In Deck": "Cards To See" }: { cardsInDeck.toString() }</p>
         </button>
     )
 }
