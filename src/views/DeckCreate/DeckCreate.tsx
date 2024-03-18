07c7adfd1cf4ff2193a0afe50ad5a80ece074563
krystiano13 <crystianotv@gmail.com>
2024-03-18 10:02:10 +0100

    create appear animation for deck-create form

--- src/views/DeckCreate/DeckCreate.tsx
+++ src/views/DeckCreate/DeckCreate.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 export function DeckCreate() {
     return (
-        <section id="content" className="h-[90vh] p-3 flex justify-center items-center appear1">
-            <form className="w-[90%] p-3 gap-12 rounded-lg flex flex-col justify-center items-center">
+        <section id="content" className="h-[90vh] p-3 flex justify-center items-center">
+            <form className="w-[90%] p-3 gap-12 rounded-lg flex flex-col justify-center items-center appear2">
                 <input
                     className="w-full bg-transparent text-lg md:text-xl p-1 text-white outline-0 border-b-solid border-b-[1px] border-white"
                     placeholder="Your deck name"
