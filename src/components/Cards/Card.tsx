000fc960c811a4ad013adadab4add318adbbebed
krystiano13 <crystianotv@gmail.com>
2024-03-19 13:59:10 +0100

    implement mapping cards in Cards view

--- src/components/Cards/Card.tsx
+++ src/components/Cards/Card.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
-export function Card() {
+interface Props {
+    title: string
+}
+
+export const Card:React.FC<Props> = ({ title }) => {
     return (
         <button
             className="w-full appear2 glassomorph1 flex flex-col gap-2 md:gap-4 rounded-lg border-solid border-[1px] p-2">
-            <h2 className="text-txt text-xl md:text-2xl font-medium">Test Card</h2>
+            <h2 className="text-txt text-left text-xl md:text-2xl font-medium">{ title.slice(0,24) + (title.length > 24 ? "..." : "") }</h2>
             <section id="buttons" className="flex gap-3">
                 <button className="text-white font-semibold p-0.5 pl-4 pr-4 bg-emerald-500">Edit</button>
                 <button className="text-white font-semibold p-0.5 pl-4 pr-4 bg-red-500">Delete</button>
--- src/views/Cards/Cards.tsx
+++ src/views/Cards/Cards.tsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useContext} from "react";
+import { DeckContext } from "../../contexts/DeckContext";
 import { Card } from "../../components/Cards/Card";
 
 interface Props {
     deck: number|undefined
 }
 
-export const Cards:React.FC<Props> = () => {
+export const Cards:React.FC<Props> = ({ deck }) => {
+    const deckContext = useContext(DeckContext);
     return (
         <section id="content" className="h-[90vh] p-3 flex flex-col gap-4">
-            <Card />
+            <Card title="siema z rana siema z rana" />
+            {
+                deck && deckContext[deck].cards.map(item => (
+                    <Card title="siema" />
+                ))
+            }
             <button
                 className="w-full appear2 glassomorph1 text-white font-semibold text-lg rounded-lg border-solid border-[1px] p-2">
                 +
