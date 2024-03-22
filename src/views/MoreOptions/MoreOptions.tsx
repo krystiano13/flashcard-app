343b57f415e9062614293c8b36bc489784cda48c
krystiano13 <crystianotv@gmail.com>
2024-03-22 13:57:55 +0100

    create styles for MoreOptions view

--- src/views/MoreOptions/MoreOptions.tsx
+++ src/views/MoreOptions/MoreOptions.tsx
@@ -12,11 +12,15 @@ export function MoreOptions() {
     },[]);
 
     return (
-        <section id="content" className="h-[90vh] p-3 flex flex-col gap-4">
-            <NavLink to="/">
+        <section id="content" className="h-[90vh] p-3 flex flex-col justify-center items-center gap-4 md:gap-9">
+            <NavLink to="/"
+                className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
+            >
                 Rename Deck
             </NavLink>
-            <button>
+            <button
+                className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
+            >
                 Delete Deck
             </button>
         </section>
