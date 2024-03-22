a8cc68a5381e244eb696333bf4f49d548d2c06ad
krystiano13 <crystianotv@gmail.com>
2024-03-22 14:10:41 +0100

    add loading for deleting decks and 'back' button in MoreOptions view

--- src/views/MoreOptions/MoreOptions.tsx
+++ src/views/MoreOptions/MoreOptions.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useSearchParams, useNavigate, NavLink } from "react-router-dom";
 import { DeckSaveContext, DeckContext } from "../../contexts/DeckContext";
 import { setData } from "../../utils/storage";
+import { Spinner } from "../../components/Spinner/Spinner";
 
 export function MoreOptions() {
     const [params, setParams] = useSearchParams();
+    const [loading, setLoading] = useState<boolean>(false);
 
     const navigate = useNavigate();
     const deckSave = useContext(DeckSaveContext);
@@ -18,6 +20,7 @@ export function MoreOptions() {
 
     async function deleteDeck() {
         const deckID:number = Number(params.get('deck'));
+        setLoading(true);
 
         return new Promise(resolve => {
             const decksArray = decks.filter(item => item.id !== deckID);
@@ -29,17 +32,31 @@ export function MoreOptions() {
 
     return (
         <section id="content" className="h-[90vh] p-3 flex flex-col justify-center items-center gap-4 md:gap-9">
-            <NavLink to="/"
-                className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
-            >
-                Rename Deck
-            </NavLink>
-            <button
-                onClick={() => {deleteDeck().then(() => navigate(`/`))}}
-                className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
-            >
-                Delete Deck
-            </button>
+           { loading && <Spinner /> }
+           { 
+           !loading && 
+            <>
+                <NavLink to="/"
+                    className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
+                >
+                    Rename Deck
+                </NavLink>
+                <button
+                    onClick={() => {deleteDeck().then(() => {
+                        setLoading(false);
+                        navigate(`/`);
+                    })}}
+                    className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
+                >
+                    Delete Deck
+                </button>
+                <NavLink to="/"
+                    className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
+                >
+                    Go Back
+                </NavLink>
+           </> 
+           }
         </section>
     )
 }
