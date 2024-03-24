0691a4cf37895d352ee3598b22a791eee06cf141
krystiano13 <crystianotv@gmail.com>
2024-03-24 20:44:13 +0100

    implement card flip

--- src/views/Review/Review.tsx
+++ src/views/Review/Review.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import './Review.css';
 
 export function Review() {
+    const [ flip, setFlip ] = useState<boolean>(false);
+
     return (
         <section id="content" className="h-[90vh] p-3 flex justify-center items-center">
-            <div id="card" className="w-[80%] h-auto min-h-[70vh] relative">
-                <section id="oneside" className="w-full p-1 absolute h-auto bg-purple-600 rounded-lg">
+            <div onClick={() => setFlip(true)} id="card" className="w-[80%] h-auto min-h-[70vh] relative">
+                <section id="oneside" className={`${flip ? "flip3" : "flip1"} w-full p-1 absolute h-full bg-purple-600 rounded-lg`}>
                     <p className="text-white text-center text-lg h-full">
                         Lorem Ipsum ?
                     </p>
                 </section>
-                <section id="secondside" className="w-full p-1 absolute h-full bg-purple-600 rounded-lg">
+                <section id="secondside" className={`${flip ? "flip4" : "flip2"} w-full p-1 absolute h-full bg-purple-600 rounded-lg`}>
                     <p className="text-white text-center text-lg">
                         Dolor ...
                     </p>
