641aa0a4d4024d0eef2368d6e82d97b4036b0b74
krystiano13 <crystianotv@gmail.com>
2024-03-24 20:08:43 +0100

    create basic structure of card component

--- src/views/Review/Review.tsx
+++ src/views/Review/Review.tsx
@@ -2,6 +2,19 @@ import React from "react";
 
 export function Review() {
     return (
-        <></>
+        <section id="content" className="h-[90vh] p-3 flex justify-center items-center">
+            <div id="card" className="w-[80%] h-auto min-h-[70vh] relative">
+                <section id="oneside" className="w-full p-1 absolute h-auto bg-purple-600 rounded-lg">
+                    <p className="text-white text-center text-lg h-full">
+                        Lorem Ipsum ?
+                    </p>
+                </section>
+                <section id="secondside" className="w-full p-1 absolute h-full bg-purple-600 rounded-lg">
+                    <p className="text-white text-center text-lg">
+                        Dolor ...
+                    </p>
+                </section>
+            </div> 
+        </section>
     )
 }
