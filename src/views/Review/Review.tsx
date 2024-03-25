8390fe747a7fd439282dd3d7446bfcc7c3ee0c4c
krystiano13 <crystianotv@gmail.com>
2024-03-25 09:54:55 +0100

    create styles for tablet version of review view

--- src/views/Review/Review.tsx
+++ src/views/Review/Review.tsx
@@ -15,20 +15,20 @@ export function Review() {
     return (
         <section id="content" className="h-[90vh] p-3 flex justify-center items-center">
             <div onClick={() => setFlip(true)} id="card" className="w-[80%] h-auto min-h-[70vh] relative">
-                <section id="oneside" className={`${flip ? "flip3" : "flip1"} w-full p-5 absolute h-full gradient2 rounded-lg overflow-y-scroll`}>
-                    <p className="text-white text-center text-lg">
+                <section id="oneside" className={`${flip ? "flip3" : "flip1"} w-full p-5 absolute h-full gradient2 rounded-lg overflow-y-auto`}>
+                    <p className="text-white text-center text-lg md:text-2xl">
                         Lorem Ipsum ?
                     </p>
                 </section>
-                <section id="secondside" className={`${flip ? "flip4" : "flip2"} w-full p-5 absolute gradient2 h-full rounded-lg`}>
-                    <p className="text-white text-center text-lg">
+                <section id="secondside" className={`${flip ? "flip4" : "flip2"} w-full p-5 absolute gradient2 h-full rounded-lg overflow-y-auto`}>
+                    <p className="text-white text-center text-lg md:text-2xl">
                         Dolor ...
                     </p>
                 </section>
             </div> 
-            <div id="review" className={`fixed w-[100vw] flex flex-col items-center justify-end h-[85vh] pointer-events-none`}>
-                <button className={`transition bg-primary-dark w-4/5 text-white p-2 ${!reviewBtn && 'translate-y-[100vh]'}`}>Review</button>
-            </div>
+            <button className={`fixed transition md:text-xl bg-primary-dark w-4/5 md:w-3/5 text-white p-2 md:p-3 ${reviewBtn && 'translate-y-[40vh]'} ${!reviewBtn && 'translate-y-[100vh]'}`}>
+                Review
+            </button>
         </section>
     )
 }
