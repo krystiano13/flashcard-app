34746ff8ce258904fce7b50e99f9fdbbbc9b7142
krystiano13 <crystianotv@gmail.com>
2024-03-25 09:32:54 +0100

    improve experience of reading card with huge amount of text

--- src/views/Review/Review.tsx
+++ src/views/Review/Review.tsx
@@ -7,12 +7,12 @@ export function Review() {
     return (
         <section id="content" className="h-[90vh] p-3 flex justify-center items-center">
             <div onClick={() => setFlip(true)} id="card" className="w-[80%] h-auto min-h-[70vh] relative">
-                <section id="oneside" className={`${flip ? "flip3" : "flip1"} w-full p-1 absolute h-full gradient2 rounded-lg flex justify-center items-center`}>
+                <section id="oneside" className={`${flip ? "flip3" : "flip1"} w-full p-5 absolute h-full gradient2 rounded-lg overflow-y-scroll`}>
                     <p className="text-white text-center text-lg">
                         Lorem Ipsum ?
                     </p>
                 </section>
-                <section id="secondside" className={`${flip ? "flip4" : "flip2"} w-full p-1 absolute gradient2 h-full rounded-lg flex justify-center items-center`}>
+                <section id="secondside" className={`${flip ? "flip4" : "flip2"} w-full p-5 absolute gradient2 h-full rounded-lg`}>
                     <p className="text-white text-center text-lg">
                         Dolor ...
                     </p>
