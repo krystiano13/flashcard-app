7d15d53321e6481a4052dc43c11d119279fdaa44
krystiano13 <crystianotv@gmail.com>
2024-03-28 10:23:34 +0100

    implement data erasure

--- src/views/Settings/Settings.tsx
+++ src/views/Settings/Settings.tsx
@@ -1,16 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { DeckSaveContext, DeckContext } from "../../contexts/DeckContext";
+import { setData } from "../../utils/storage";
 
 export function Settings() {
     const [modal, setModal] = useState<boolean>(false);
+    const decks = useContext(DeckContext);
+    const setDecks = useContext(DeckSaveContext);
+
+    function erase() {
+        return new Promise(resolve => {
+            setDecks([]);
+            resolve('resolved');
+        })
+        .then(() => {
+            setData(decks);
+        })
+        .then(() => setModal(false))
+    }
+
     return (
         <>
         <section id="content" className="h-[90vh] p-3 flex flex-col gap-4">
             <button
                 onClick={() => setModal(true)}
                 className="w-full md:w-[70%] text-center appear2 glassomorph1 text-white font-semibold text-lg md:text-2xl rounded-lg border-solid border-[1px] p-2"
-                >
-                    Erase All Data
-                </button>
+            >
+                Erase All Data
+            </button>
         </section>
         {
             modal &&
@@ -18,7 +34,7 @@ export function Settings() {
                 className="appear1 fixed w-full h-full flex flex-col items-center justify-center gap-6 glassomorph1"
             >
                 <h2 className="text-white font-semibold text-lg md:text-2xl">Are you sure ?</h2>
-                <button className={`md:text-2xl text-xl bg-emerald-700 w-4/5 md:w-3/5 text-white p-2 md:p-3`}>
+                <button onClick={erase} className={`md:text-2xl text-xl bg-emerald-700 w-4/5 md:w-3/5 text-white p-2 md:p-3`}>
                     Yes
                 </button>
                 <button onClick={() => setModal(false)} className={`md:text-2xl text-xl bg-red-700 w-4/5 md:w-3/5 text-white p-2 md:p-3`}>
