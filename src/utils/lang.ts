d29b9100ca21271dc332634a948ebbd39ce8c7ac
krystiano13 <crystianotv@gmail.com>
2024-03-29 09:25:52 +0100

    create language data for home and learn views

---
+++ src/utils/lang.ts
@@ -0,0 +1,15 @@
+export const lang = {
+    home: {
+        cards: ['Cards In Deck', "Karty W Talii"],
+        name: ['deck name', 'nazwa talii']
+    },
+    learn: {
+        cards: ['Cards To See', "Karty Do Obejrzenia"],
+        noCards: ['You have no cards to check for now', 'Aktualnie nie masz kart do przejrzenia'],
+        review: ['Review', 'Oceń'],
+        remember: ['Did you remember the answer ?', 'Zapamiętałeś odpowiedź ?'],
+        no: ['No', 'Nie'],
+        partially: ['Partially', 'Częściowo'],
+        yes: ['Yes', 'Tak']
+    }
+}
