4014a6225c6c337d39f8dd198b538b63365ac181
krystiano13 <crystianotv@gmail.com>
2024-03-30 10:00:01 +0100

    create file that stores data about pages in help section

---
+++ src/utils/helpData.ts
@@ -0,0 +1,36 @@
+//@ts-ignore
+import homeIcon from "../../public/icons/home-icon.svg";
+//@ts-ignore
+import addIcon from "../../public/icons/plus-icon.svg";
+//@ts-ignore
+import settingsIcon from "../../public/icons/settings-icon.svg";
+//@ts-ignore
+import bookIcon from '../../public/icons/book-icon.svg';
+
+export const helpData = [
+    {
+        title: ["Help", "Pomoc"],
+        description: ["Welcome to the help section. In next pages we'll show you everything about all sections of the app", "Witamy w sekcji pomocy. Na następnych stronach dowiesz się o wszystkich elementach aplikacji."],
+        icon: false
+    },
+    {
+        title: "",
+        description: "",
+        icon: ""
+    },
+    {
+        title: "",
+        description: "",
+        icon: ""
+    },
+    {
+        title: "",
+        description: "",
+        icon: ""
+    },
+    {
+        title: "",
+        description: "",
+        icon: ""
+    },
+]
