b4984fcf4750c9701f259b3bb239fda52da9c3f3
krystiano13 <crystianotv@gmail.com>
2024-03-30 10:07:35 +0100

    finish adding data to the help section

--- src/utils/helpData.ts
+++ src/utils/helpData.ts
@@ -10,27 +10,42 @@ import bookIcon from '../../public/icons/book-icon.svg';
 export const helpData = [
     {
         title: ["Help", "Pomoc"],
-        description: ["Welcome to the help section. In next pages we'll show you everything about all sections of the app", "Witamy w sekcji pomocy. Na następnych stronach dowiesz się o wszystkich elementach aplikacji."],
+        description: [
+            "Welcome to the help section. In next pages we'll show you everything about all sections of the app", 
+            "Witamy w sekcji pomocy. Na następnych stronach dowiesz się o wszystkich elementach aplikacji."
+        ],
         icon: false
     },
     {
-        title: "",
-        description: "",
-        icon: ""
+        title: ["Home Section", "Sekcja Domowa"],
+        description: [
+            "In the home section, you can browse your decks, edit them, and delete them. You can also start custom learning.",
+            "W sekcji domowej możesz przeglądać twoje talię, edytować je, oraz usuwać. Możesz także rozpocząć naukę niestandardową"
+        ],
+        icon: homeIcon
     },
     {
-        title: "",
-        description: "",
-        icon: ""
+        title: ["Learn Section", "Sekcja Nauki"],
+        description: [
+            "",
+            ""
+        ],
+        icon: bookIcon
     },
     {
-        title: "",
-        description: "",
-        icon: ""
+        title: ["Creation Section", "Sekcja Tworzenia"],
+        description: [
+            "In the learning section, you can browse cards that require a reminder.",
+            "W sekcji nauki możesz przeglądać karty, które wymagają przypomnienia"
+        ],
+        icon: addIcon
     },
     {
-        title: "",
-        description: "",
-        icon: ""
+        title: ["Settings Section", "Sekcja Ustawień"],
+        description: [
+            "In the settings section, you can clear the application data, launch the help window again, and change the language",
+            "W sekcji ustawień możesz wyczyścić dane aplikacji, odpalić okno pomocy jeszcze raz oraz zmienić język"
+        ],
+        icon: settingsIcon
     },
 ]
