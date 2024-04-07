f54a7d30e1cb3fe58f305e58db135178fba5667f
krystiano13 <crystianotv@gmail.com>
2024-04-07 15:10:02 +0200

    create function for initializing admob

---
+++ src/utils/admob.ts
@@ -0,0 +1,23 @@
+import {AdMob, AdmobConsentStatus} from "@capacitor-community/admob";
+
+export async function initialize(): Promise<void> {
+    await AdMob.initialize();
+
+    const [trackingInfo, consentInfo] = await Promise.all([
+        AdMob.trackingAuthorizationStatus(),
+        AdMob.requestConsentInfo(),
+    ]);
+
+    if (trackingInfo.status === 'notDetermined') {
+        await AdMob.requestTrackingAuthorization();
+    }
+
+    const authorizationStatus = await AdMob.trackingAuthorizationStatus();
+    if (
+        authorizationStatus.status === 'authorized' &&
+        consentInfo.isConsentFormAvailable &&
+        consentInfo.status === AdmobConsentStatus.REQUIRED
+    ) {
+        await AdMob.showConsentForm();
+    }
+}
